d1d0b524ffdcd5459e2145a188277d812328f804
isarubio95 <isarubio95@gmail.com>
2026-01-19 12:09:19 +0100

    formulario de inicio de sesión añadido

--- djangoproject/mysite/urls.py
+++ djangoproject/mysite/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from exampleapp.views import homeView
 import django.contrib.auth.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", homeView, name="home"),
-    path('accounts/', django.contrib.auth.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
