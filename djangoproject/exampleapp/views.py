85744cd7455c37b8cb3a7a1c46dd0c2342fb9f65
isarubio95 <isarubio95@gmail.com>
2026-01-20 18:31:07 +0100

    vista de formularios de inicioi de sesión añadida

--- djangoproject/exampleapp/views.py
+++ djangoproject/exampleapp/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import ActividadForm
+from .forms import ActividadForm, DaisySignupForm
 from .models import Actividad
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = DaisySignupForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
